2c6df37c1b9eb86dee7a3d1a22865a569417054e
Jordan Hanna <jhanna01@uw.edu>
2013-05-21 22:32:32 -0400

    Added node edge visualization, and JSON fix.
    * Added node edges, but need to span them out more and make some other tweeks.
    
    * Fixed the "IP" syntax for .JSON

--- visualize.js
+++ visualize.js
@@ -1,5 +1,5 @@
-var width = 960,
-	height = 500,
+var width = 1100,
+	height = 600,
 	dataSet = {};
 
 var color = d3.scale.category20();
@@ -16,7 +16,8 @@ var svg = d3.select("#chart").append("svg")
 	.attr("height", height);
 
 // Use the json file for the graph data
-d3.json("visualization.json", function(json) {
+d3.json("visualization.json", function(error, json) {
+	console.log("e: " + error);
     dataSet = json;
     visualizeIt();
     return 0;
@@ -24,7 +25,6 @@ d3.json("visualization.json", function(json) {
 
 
 function visualizeIt() {
-
 	force
 		.nodes(dataSet.nodes)
 		.links(dataSet.links)
@@ -34,22 +34,35 @@ function visualizeIt() {
 		.data(dataSet.links)
 	  .enter().append("line")
 		.attr("class", "link")
-		.style("stroke-width", function(d) { return (Math.sqrt(d.value) * 1.5 ); });
+		.style("stroke-width", function(d, i) { return (Math.sqrt(d.size)); })
+		.style("stroke", "#999")
+		.style("stroke-opacity", .7)
+		.attr("marker-end", "url(#end)");
+
 
 	var node = svg.selectAll(".node")
 		.data(dataSet.nodes)
 	  .enter().append("circle")
 	  	.attr("class", "node")
 	  	// Here we will change nodes radius
-	  	.attr("r", 5)
+	  	.attr("r", 10)
 	  	//.style("fill", function(d) { return color(d.group)})
+	  	.style("stroke-width", 1.5);
 	  	.call(force.drag);
 
 	// Node mouse-over label
 	node.append("title")
 		.text(function(d) { return d.IP; });
 
-	
+	force.on("tick", function() {
+     link.attr("x1", function(d, i) { return d.source.x; })
+        .attr("y1", function(d, i) { return d.source.y; })
+        .attr("x2", function(d, i) { return d.target.x; })
+        .attr("y2", function(d, i) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  	});
 
 }
 
